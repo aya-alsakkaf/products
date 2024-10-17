8b717faf15449b0d642f02bdb65167eed2a03810
Aya Alsakkaf <aya.alsakkaf@ccse.ku.edu.kw>
2024-10-17 11:57:29 +0300

    done

--- src/api/product.js
+++ src/api/product.js
@@ -9,4 +9,36 @@ const getAllProducts = async () => {
   }
 };
 
-export { getAllProducts };
+const getProductById = async (id) => {
+  try {
+    const { data } = await instance.get(`/mini-project/api/items/${id}`);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const createProduct = async (categoryID, productInfo) => {
+  try {
+    const formData = new FormData();
+    for (key in productInfo) {
+      if (key != "image") formData.append(key, productInfo[key]);
+    }
+
+    formData.append("image", {
+      name: "image.jpg",
+      type: "image/jpeg",
+      uri: productInfo.image,
+    });
+
+    const { data } = await instance.post(
+      `/mini-project/api/items/${categoryID}`,
+      formData
+    );
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { getAllProducts, getProductById, createProduct };
